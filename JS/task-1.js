d9ffe1ea4186d6d3eb62cd1400444ec55786b164
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2020-08-29 10:07:45 +0300

    добавляет копию с предыдущей домашки.

---
+++ JS/task-1.js
@@ -0,0 +1,11 @@
+"use strict";
+let result = "";
+const getItemsString = function (array) {
+  for (let i = 0; i < array.length; i += 1) {
+    result += `${i + 1} - ${array[i]}\n`;
+  }
+  return result;
+};
+
+console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+console.log(getItemsString([5, 10, 15]));
---
+++ JS/tests.js
@@ -0,0 +1,17 @@
+"use strict";
+// let age = 91;
+
+// // console.log(age >= 14 && age <= 90);
+// console.log(!age >= 14 && !age >= 90);
+
+const hotel = {
+  name: "Resort Hotel",
+  stars: 5,
+  capacity: 100,
+};
+const keys = Object.keys(hotel);
+const values = Object.values(hotel);
+const entries = Object.entries(hotel);
+console.log(keys);
+console.log(values);
+console.log(entries);
